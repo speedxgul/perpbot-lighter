96c64e00caa90928afceb900fae371019b71e445
raveeshcool <raveesh_g@me.iitr.ac.in>
2025-11-07 22:00:33 +0530

    continuing progess, restarting coding discipline streak

--- index.ts
+++ index.ts
@@ -1,1 +1,17 @@
-console.log("Hello via Bun!");
+import { CandlestickApi, IsomorphicFetchHttpLibrary, ServerConfiguration } from "./lighter-sdk-ts/generated";
+const BASE_URL = "https://mainnet.zklighter.elliot.ai"
+const SOL_MARKET_ID = 2
+
+async function getKlines(marketId: number) {
+    const klinesApi = new CandlestickApi({
+        baseServer: new ServerConfiguration<{  }>(BASE_URL, {  }),
+        httpApi: new IsomorphicFetchHttpLibrary(),
+        middleware: [],
+        authMethods: {}
+    });
+
+    const klines = await klinesApi.candlesticks(SOL_MARKET_ID, '3m', Date.now() - 1000 * 60 * 60 * 24, Date.now(), 50, false);
+    console.log(klines);
+}
+
+getKlines(SOL_MARKET_ID);
