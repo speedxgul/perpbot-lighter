b40f6ef9c5d27b7fc2dbaacaab367d87c593fef3
raveeshcool <raveesh_g@me.iitr.ac.in>
2026-03-07 03:44:25 +0530

    tests

--- createPosition.ts
+++ createPosition.ts
@@ -1,6 +1,6 @@
 import { NonceManagerType } from "./lighter-sdk-ts/nonce_manager";
 import { SignerClient } from "./lighter-sdk-ts/signer";
-import { AccountApi, ApiKeyAuthentication, IsomorphicFetchHttpLibrary, OrderApi, ServerConfiguration } from "./lighter-sdk-ts/generated";
+import { AccountApi, ApiKeyAuthentication, CandlestickApi, IsomorphicFetchHttpLibrary, OrderApi, ServerConfiguration } from "./lighter-sdk-ts/generated";
 import type { Account } from "./accounts";
 import { API_KEY_INDEX, BASE_URL } from "./config";
 import { MARKETS } from "./markets";
@@ -18,14 +18,26 @@ export async function CreatePosition(account:Account,symbol:string,side:"LONG"|"
 
     const market = MARKETS[symbol as keyof typeof MARKETS];
 
-// fetch latestPrice or just find a market order func
+    const candleStickApi = new CandlestickApi({
+        baseServer: new ServerConfiguration<{  }>(BASE_URL, {  }),
+        httpApi: new IsomorphicFetchHttpLibrary(),
+        middleware: [],
+        authMethods: {}
+    });
+     const candleStickData = await candleStickApi.candlesticks(market.marketId, '1m', Date.now() - 1000 * 60 * 5, Date.now(), 1, false)
+     const latestPrice = candleStickData.candlesticks[candleStickData.candlesticks.length - 1]?.close;
+     if (!latestPrice) {
+        throw new Error("No latest price found");
+     }
+     console.log(latestPrice)
+
     await client.createOrder({
         marketIndex: market.marketId,
         clientOrderIndex: market.clientOrderIndex,// need to pass this for model to remember openedPosition Index
         baseAmount: quantity * market.priceDecimals,
         price: (side == "LONG" ? latestPrice * 1.01 : latestPrice * 0.99) * market.priceDecimals,
         isAsk: side == "LONG" ? false : true,
-        orderType: SignerClient.ORDER_TYPE_LIMIT,
+        orderType: SignerClient.ORDER_TYPE_MARKET, // market
         timeInForce: SignerClient.ORDER_TIME_IN_FORCE_GOOD_TILL_TIME,
         reduceOnly: 0,
         triggerPrice: SignerClient.NIL_TRIGGER_PRICE,
--- llm_stuff.ts
+++ llm_stuff.ts
@@ -3,7 +3,7 @@ import { streamText, tool } from 'ai';
 import { z } from 'zod';
 import { PROMPT } from './prompt';
 import { getOpenOrders } from './getPositions';
-import type { Account } from './accounts';
+import { SUPPORTED_ACCOUNTS, type Account } from './accounts';
 import { getPortfolio } from './getPortfolio';
 import { getIndicators } from './stockPrices';
 import { MARKETS } from './markets';
@@ -16,14 +16,14 @@ export const invokeAgent = async (account:Account) => {
         apiKey: process.env['OPEN_ROUTER_API_KEY'] ?? '',
     });
      
-    let ALL_INDICATOR_DATA = "";
-    const indicators = await Promise.all(
+    
+    const arrayOfIndicatorsData = await Promise.all( 
         (Object.keys(MARKETS) as (keyof typeof MARKETS)[]).map(async (marketSlug) => {
-            console.log(marketSlug)
+          
             const intradayIndicators = await getIndicators("5m", MARKETS[marketSlug].marketId);
             const longTermIndicators = await getIndicators("4h", MARKETS[marketSlug].marketId); 
     
-    ALL_INDICATOR_DATA = ALL_INDICATOR_DATA + `
+    return `
     MARKET - ${marketSlug}
     Intraday (5m candles) (oldest → latest):
     Mid prices - [${intradayIndicators.midPrices.join(",")}]
@@ -36,33 +36,28 @@ export const invokeAgent = async (account:Account) => {
     MACD - [${longTermIndicators.macd.join(",")}]
 
     `
+  
   }))
+  const ALL_INDICATOR_DATA = arrayOfIndicatorsData.join("\n");
 //   console.log(indicators)
 
 
     const openPositions = await getOpenOrders(account)
+    console.log(openPositions)
     const portfolio = await getPortfolio(account)
     //prisma model invocation
     const enrichedPrompt = PROMPT.replace("{{INVOKATION_TIMES}}", "0")// what is invocation count, fix this
     .replace("{{OPEN_POSITIONS}}", openPositions?.map((position) => `${position.symbolName} ${position.position} ${position.sign}`).join(", ") ?? "")
     .replace("{{PORTFOLIO_VALUE}}", `$${portfolio.available}`)
-    .replace("{{ALL_INDICATOR_DATA}}", ALL_INDICATOR_DATA)
+    .replace("{{ALL_INDICATOR_DATA}}", ALL_INDICATOR_DATA ?? "")// if promise rejected then make a condition?
     .replace("{{AVAILABLE_CASH}}", `$${portfolio.available}`)
     .replace("{{CURRENT_ACCOUNT_VALUE}}", `$${portfolio.available}`)
     .replace("{{CURRENT_ACCOUNT_POSITIONS}}", JSON.stringify(openPositions))
-  
-    console.log(enrichedPrompt)
+    // console.log(enrichedPrompt)
 //create 2 tools, createPositions and closeAllPosition
     const response = streamText({
         model: openrouter(account.modelName),
-        prompt: PROMPT.replace('{{OPEN_POSITIONS}}', JSON.stringify(OpenOrders))
-        .replace('{{INVOCATION_TIMES}}',"0")// will come from a DB eventually
-        .replace('{{PORTFOLIO_VALUE}}', JSON.stringify(getPortfolio(account).then((res) => {
-            return res;
-        })))
-        .replace("{{INTRADAY_POSITIONS}}",intradayIndicators.midPrices.join(","))
-        .replace("{{LONG_TERM_POSITIONS}}",longTermIndicators.midPrices.join(","))
-        
+        prompt: enrichedPrompt,
         tools:{
             createPosition: {
                 description: 'Open a position in the given market',
@@ -73,27 +68,24 @@ export const invokeAgent = async (account:Account) => {
                 }),
                 execute: async ({ symbol, side, quantity }) => {
                   // Do the opposite of what the AI infers
-                  side = side === "LONG" ? "SHORT" : "LONG";
+                
                   await CreatePosition(account, symbol, side, quantity);
-                  await prisma.toolCalls.create({
-                    data: {
-                      invocationId: modelInvocation.id,
-                      toolCallType: ToolCallType.CREATE_POSITION,
-                      metadata: JSON.stringify({ symbol, side, quantity }),
-                    },
-                  });
                   return `Position opened successfully for ${quantity} ${symbol}`;
+                  // await prisma.toolCalls.create({
+                  //   data: {
+                  //     invocationId: modelInvocation.id,
+                  //     toolCallType: ToolCallType.CREATE_POSITION,
+                  //     metadata: JSON.stringify({ symbol, side, quantity }),
+                  //   },
+                  // });
                 },
               },
 
 }
-            
-
-        
-
-
     });
 
     await response.consumeStream();
     return response.text;
 };
+
+invokeAgent(SUPPORTED_ACCOUNTS[0]!)
